9c6b7e56fcb0c7f3a5ee62c6e5a9d1b4b8f74473
Tyler Skalka <86090906+Tskalka@users.noreply.github.com>
2022-07-02 19:40:51 -0600

    Add files via upload

---
+++ Python Files/solution1_stacks.py
@@ -0,0 +1,20 @@
+driveway = []
+driveway.append("car 1")
+driveway.append("car 2")
+driveway.pop()
+driveway.append("car 3")
+driveway.append("car 4")
+driveway.pop()
+driveway.pop()
+driveway.append("car 5")
+driveway.append("car 6")
+driveway.pop()
+driveway.append("car 7")
+driveway.pop()
+
+print(driveway)
+# driveway = ['car 1', 'car 5']
+
+# 1. You have 2 cars in your driveway
+# 2. car 1 and car 5
+# 3. Order is car 1 and car 5
